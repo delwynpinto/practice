cfc0a92316c988af44c70ee3440b1f09164cbb5b
delwynpinto <8274976+delwynpinto@users.noreply.github.com>
2020-03-25 10:29:13 +0530

    Added route to home page. Modified first page to be Home. Incremented version.

--- Changelog.md
+++ Changelog.md
@@ -1,5 +1,9 @@
 ##Changes
 
+##0.0.2
+Added route to home page.
+Modified first page to be Home.
+
 ##0.0.1
 Added app routing module
 Added routes to router module
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { ImageCropperComponent } from './image-cropper/image-cropper.component';
 
 const routes: Routes = [
-  { path: 'image-cropper', component: ImageCropperComponent }
+  { path: 'image-cropper', component: ImageCropperComponent },
+  { path: '', component: HomeComponent }
 ]
 
 @NgModule({
